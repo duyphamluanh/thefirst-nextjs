a1c642eae697ea93fe31949707937769932ed6c9
duyphamluanh <anhduy123000@gmail.com>
2024-06-11 16:35:21 +0700

    Add pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -22,8 +22,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   // Inside the Search Component...
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
-  
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
